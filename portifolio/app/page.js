7c3f6abfbae1806f313cbd17f5993ca4f9b7d440
HenriqueFrancaa <henrique23.franca@gmail.com>
2025-04-12 20:59:59 -0300

    Terminando design do portifolio

--- portifolio/app/page.js
+++ portifolio/app/page.js
@@ -20,11 +20,72 @@ export default function Home() {
         </div>
     </div>
 
-    <div>
       <div className="text-content">
-        <h1>asdasd</h1>
+        <h1>Meus Projetos</h1>      
       </div>
-    </div>
+
+      <div className="projects">
+
+        <div className="project-box pic">
+          <h1>Workshop de Programação Competitiva</h1>
+          <img src="/maratona.webp" />
+          <p>
+            Evento de duas semanas organizado pelos próprios membros da equipe de Maratona de Programação, voltado para alunos iniciantes do 1º e 2º períodos do curso de Ciência da Computação. 
+            <br/><br/>O workshop abordou desde os conceitos básicos de programação até algoritmos mais avançados. 
+            Durante o evento, cada integrante da equipe ministrou aulas, e os participantes puderam colocar seus conhecimentos em prática por meio de competições realizadas ao longo das semanas.
+          </p>
+        </div>
+    
+        <div className="project-box pic">
+          <h1>Jogo da senha</h1>
+          <img src="/biblioteca.png"/>
+          <button>Jogar</button>
+        </div>
+
+        <div className="project-box pic">
+          <h1>Quiz de Ciência da Computação</h1>
+          <img src="/quizc3.webp"/>
+          <p>
+            O projeto consiste em um quiz voltado para alunos de Ciência da Computação, do 1º ao 5º período, com perguntas baseadas nos conteúdos das disciplinas. 
+            <br/><br/>A proposta é utilizar a gamificação como ferramenta para tornar o aprendizado mais leve, divertido e interativo, incentivando os estudantes a revisarem os conteúdos de forma mais engajada e motivadora
+          </p>
+
+        </div>
+
+      </div>
+
+      <div className="skills-tools">
+        <div className="text-content">
+          <h1>Linguagens de Programação</h1>
+          <div className="skills skills-icons">
+            <h3 className="pic">C++</h3>
+            <h3 className="pic">C</h3>
+            <h3 className="pic">Python</h3>
+            <h3 className="pic">Java</h3>
+            <h3 className="pic">HTML</h3>
+            <h3 className="pic">CSS</h3>
+            <h3 className="pic">JavaScript</h3>
+            <h3 className="pic">SQL</h3>
+          </div>
+        </div>
+          
+          <div className="text-content">
+            <h1>Ferramentas</h1>
+            <div className="tools tools-icons">
+              <h3 className="pic">Git</h3>
+              <h3 className="pic">Vim</h3>
+              <h3 className="pic">VsCode</h3>
+              <h3 className="pic">React</h3>
+              <h3 className="pic">Notion</h3>
+              <h3 className="pic">Figma</h3>
+              <h3 className="pic">MySQL</h3>
+
+
+            </div>
+          </div>
+      </div>
+
+
 
     </>
   );
